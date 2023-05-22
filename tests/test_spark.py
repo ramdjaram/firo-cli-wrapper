fcad9afb9ca212838e79efe664e6113180b704d3
ramdjaram <milanranisavljevic@Milans-Mac-mini.local>
2023-05-23 00:18:42 +0200

    Mark tests with no args

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -1,19 +1,27 @@
+import pytest
+
+
+@pytest.mark.noargs
 def test_listunspentsparkmints(firo_cli):
     firo_cli.listunspentsparkmints()
 
 
+@pytest.mark.noargs
 def test_listsparkmints(firo_cli):
     firo_cli.listsparkmints()
 
 
+@pytest.mark.noargs
 def test_listsparkspends(firo_cli):
     firo_cli.listsparkspends()
 
 
+@pytest.mark.noargs
 def test_getsparkdefaultaddress(firo_cli):
     firo_cli.getsparkdefaultaddress()
 
 
+@pytest.mark.noargs
 def test_getallsparkaddresses(firo_cli):
     firo_cli.getallsparkaddresses()
 
@@ -36,10 +44,6 @@ def test_resetsparkmints(firo_cli):
 
 def test_setsparkmintstatus(firo_cli):
     firo_cli.setsparkmintstatus()
-
-
-def test_resetsparkmints(firo_cli):
-    firo_cli.resetsparkmints()
 
 
 def test_mintspark(firo_cli):
