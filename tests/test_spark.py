248e3f91bb6edec14f72db764bcf5fed9fa98de8
ramdjaram <milanranisavljevic@Milans-Mac-mini.local>
2023-05-23 00:14:57 +0200

    Add test_spark.py with all template commands

---
+++ tests/test_spark.py
@@ -0,0 +1,54 @@
+def test_listunspentsparkmints(firo_cli):
+    firo_cli.listunspentsparkmints()
+
+
+def test_listsparkmints(firo_cli):
+    firo_cli.listsparkmints()
+
+
+def test_listsparkspends(firo_cli):
+    firo_cli.listsparkspends()
+
+
+def test_getsparkdefaultaddress(firo_cli):
+    firo_cli.getsparkdefaultaddress()
+
+
+def test_getallsparkaddresses(firo_cli):
+    firo_cli.getallsparkaddresses()
+
+
+def test_getnewsparkaddress(firo_cli):
+    firo_cli.getnewsparkaddress()
+
+
+def test_getsparkbalance(firo_cli):
+    firo_cli.getsparkbalance()
+
+
+def test_getsparkaddressbalance(firo_cli):
+    firo_cli.getsparkaddressbalance()
+
+
+def test_resetsparkmints(firo_cli):
+    firo_cli.resetsparkmints()
+
+
+def test_setsparkmintstatus(firo_cli):
+    firo_cli.setsparkmintstatus()
+
+
+def test_resetsparkmints(firo_cli):
+    firo_cli.resetsparkmints()
+
+
+def test_mintspark(firo_cli):
+    firo_cli.mintspark()
+
+
+def test_spendspark(firo_cli):
+    firo_cli.spendspark()
+
+
+def test_lelantustospark(firo_cli):
+    firo_cli.lelantustospark()
