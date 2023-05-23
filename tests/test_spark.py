f26ad0e808af935416237697b5d84831a310b723
ramdjaram <milanranisavljevic@Milans-Mac-mini.local>
2023-05-23 20:35:50 +0200

    Tag tests with 'spark'

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -2,54 +2,66 @@ import pytest
 
 
 @pytest.mark.noargs
+@pytest.mark.spark
 def test_listunspentsparkmints(firo_cli):
     firo_cli.listunspentsparkmints()
 
 
 @pytest.mark.noargs
+@pytest.mark.spark
 def test_listsparkmints(firo_cli):
     firo_cli.listsparkmints()
 
 
 @pytest.mark.noargs
+@pytest.mark.spark
 def test_listsparkspends(firo_cli):
     firo_cli.listsparkspends()
 
 
 @pytest.mark.noargs
+@pytest.mark.spark
 def test_getsparkdefaultaddress(firo_cli):
     firo_cli.getsparkdefaultaddress()
 
 
 @pytest.mark.noargs
+@pytest.mark.spark
 def test_getallsparkaddresses(firo_cli):
     firo_cli.getallsparkaddresses()
 
 
+@pytest.mark.spark
 def test_getnewsparkaddress(firo_cli):
     firo_cli.getnewsparkaddress()
 
 
+@pytest.mark.spark
 def test_getsparkbalance(firo_cli):
     firo_cli.getsparkbalance()
 
 
+@pytest.mark.spark
 def test_getsparkaddressbalance(firo_cli):
     firo_cli.getsparkaddressbalance()
 
 
+@pytest.mark.spark
 def test_resetsparkmints(firo_cli):
     firo_cli.resetsparkmints()
 
 
+@pytest.mark.spark
 def test_setsparkmintstatus(firo_cli):
     firo_cli.setsparkmintstatus()
 
 
+@pytest.mark.spark
 def test_mintspark(firo_cli):
     firo_cli.mintspark()
 
 
+@pytest.mark.spark
 def test_spendspark(firo_cli):
     firo_cli.spendspark()
 
